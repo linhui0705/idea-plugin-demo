d82a13a15a65ad57b35ac50282a8dade21f07dc8
linhui <linhui0705@gmail.com>
2020-10-17 22:00:37 +0800

    Initial commit

---
+++ src/main/java/net/coderlin/plugin/demo/listener/MyListener.java
@@ -0,0 +1,48 @@
+package net.coderlin.plugin.demo.listener;
+
+import com.intellij.ide.AppLifecycleListener;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MyListener implements AppLifecycleListener {
+    @Override
+    public void appFrameCreated(@NotNull List<String> commandLineArgs) {
+        System.out.println("appFrameCreated");
+    }
+
+    @Override
+    public void welcomeScreenDisplayed() {
+        System.out.println("welcomeScreenDisplayed");
+    }
+
+    @Override
+    public void appStarting(@Nullable Project projectFromCommandLine) {
+        System.out.println("appStarting");
+    }
+
+    @Override
+    public void projectFrameClosed() {
+        System.out.println("projectFrameClosed");
+    }
+
+    @Override
+    public void projectOpenFailed() {
+        System.out.println("projectOpenFailed");
+    }
+
+    @Override
+    public void appClosing() {
+        System.out.println("appClosing");
+        Messages.showMessageDialog("appClosing", "Hello World", Messages.getInformationIcon());
+    }
+
+    @Override
+    public void appWillBeClosed(boolean isRestart) {
+        System.out.println("appWillBeClosed");
+        Messages.showMessageDialog("appWillBeClosed", "Hello World", Messages.getInformationIcon());
+    }
+}
